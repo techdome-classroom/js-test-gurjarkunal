7ceab31827326d354c225230cee51a9e38dbdc9f
gurjarkunal <94820141+gurjarkunal@users.noreply.github.com>
2024-01-05 13:47:18 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,7 +3,27 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    if (s == null) {
+        return true;
+    }
+    let ch = x.split('');
+    let st = [];
+    for (let i = 0; i < ch.length; i++) {
+        if (ch[i] == '(' || ch[i] == '{' || ch[i] == '[') {
+            st.push(ch[i]);
+        } else if (ch[i] == ')' || ch[i] == '}' || ch[i] == ']') {
+            if (st.length === 0) {
+                return false;
+            }
+            let top = st.pop();
+            if ((ch[i] == ')' && top != '(') ||
+                (ch[i] == '}' && top != '{') ||
+                (ch[i] == ']' && top != '[')) {
+                return false;
+            }
+        }
+    }
+    return st.length === 0;
 };
 
 module.exports = { isValid };
