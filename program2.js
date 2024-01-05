c60a52cc7a48c8e2ba4fce1aa52b224e2d1dd935
gurjarkunal <94820141+gurjarkunal@users.noreply.github.com>
2024-01-05 13:49:19 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,7 +3,27 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
+   const map = new Map();
+    map.set('I', 1);
+    map.set('V', 5);
+    map.set('X', 10);
+    map.set('L', 50);
+    map.set('C', 100);
+    map.set('D', 500);
+    map.set('M', 1000);
+    const l = s.length;
+    let output = 0;
+
+    for (let i = 0; i < l; i++) {
+        if (i < l - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))){
+            output += map.get(s.charAt(i + 1)) - map.get(s.charAt(i));
+            i++;
+        }
+        else {
+            output += map.get(s.charAt(i));
+        }
+    }
+    return output; 
 };
 
 
